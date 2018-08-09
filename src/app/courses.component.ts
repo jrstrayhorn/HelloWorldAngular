425db88585dd618eb75ebf7464ada7ea6262d34d
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-09 10:53:42 -0400

    done with 5-templates

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', // <courses> - new custom HTML element
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ getTitle() }}</h2>'
 })
 export class CoursesComponent {
+    title = "List of courses"; // field
 
+    getTitle() {
+        return this.title;
+    }
 }
