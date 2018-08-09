b200576f9f17809af6a71cc66d4c321c17eb10db
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-09 11:09:48 -0400

    done with 7-services

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -14,7 +14,26 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
     title = "List of courses"; // field
-    courses = ["course1", "course2", "course3"];
+    courses;
+
+    // Logic for calling an HTTP service
+        // dont do this here
+        // make component tightly coupled to HTTP endpoint
+        // cant write unit test against this
+        // would want to create fake HTTP service
+        
+        // another issue
+        // somewhere else we might want to get HTTP service
+        // we'd have to repeat code
+
+        // a component should not include any logic
+        // except for logic for this presentation view
+
+        // details for how to get courses should be implemented somewhere else
+        // it is not the responsibility of component to get courses
+        // just how to display courses
+
+        // should use a service for this!!
 
     getTitle() {
         return this.title;
