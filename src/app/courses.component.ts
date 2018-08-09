ca3b285c4ad32e7f9ee11c0b7f5b8ea9bfd5956b
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-09 13:47:56 -0400

    done with 3-attribute binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,9 +8,15 @@ import { CoursesService } from './course/courses.service';
         <h2>{{ title }}</h2>
         <img src="{{ imageUrl }}" />
         <img [src]="imageUrl" /> <!-- this is property binding syntax -->
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan"></td> <!-- this is attribute binding syntax -->
+            </tr>
+        </table>
     `
 })
 export class CoursesComponent {
     title = "List of courses"; // field
     imageUrl = "https://picsum.photos/400/200?image=2";
+    colSpan = 2;
 }
