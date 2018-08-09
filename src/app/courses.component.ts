608284f7679034ef7a49537cfc13bb6717eadad5
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-09 10:58:02 -0400

    done with 6-directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', // <courses> - new custom HTML element
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
     title = "List of courses"; // field
+    courses = ["course1", "course2", "course3"];
 
     getTitle() {
         return this.title;
