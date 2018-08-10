a91108bdc40f4b9c4e4387a35d0b4068f7e30303
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-10 11:12:13 -0400

    done with re-usable components, 3-input properties

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+// can also use Component, inputs metadata and list the field
+// BUT this will introduce magic strings that wont get updated
+// if you change a field name, so use @Input() decorator
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
-  isFavorite: boolean;  // defaults to false
+  @Input() isFavorite: boolean;  // defaults to false
 
   constructor() { 
   }
