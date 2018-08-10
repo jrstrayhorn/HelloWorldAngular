849d9ae8670da5671af043ea9957792203ad573b
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-10 10:50:30 -0400

    done with solution adding instructor thoughts

--- src/app/title-case.pipe.ts
+++ src/app/title-case.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TitleCasePipe implements PipeTransform {
 
-  transform(title: string, args?: any): any {
+  transform(title: string): any {
     if (!title)
       return null;
 
@@ -16,17 +16,26 @@ export class TitleCasePipe implements PipeTransform {
           if (idx == 0)
             return this.getTitleCase(word);
 
-          if (word == "of" || word == "the")
+          if (this.isPreposition(word))
             return word;
 
           return this.getTitleCase(word);
         }
       )
       .join(' ');
-    
   }
 
-  getTitleCase(word: string) : string
+  private isPreposition(word: string): boolean {
+    let prepositions = [
+      'of',
+      'the',
+      'in'
+    ]
+
+    return prepositions.includes(word);
+  }
+
+  private getTitleCase(word: string) : string
   {
       return word.substring(0, 1).toUpperCase() + word.substr(1).toLowerCase();
   }
