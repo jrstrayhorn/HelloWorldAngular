9698b2d2144b0c35694218aceececa1fcddaaf54
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-13 09:38:34 -0400

    done with directives, 11-safe traversal operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  canSave = false;
+  task = {
+    title: 'Review applications',
+    assignee: null
+  }
 }
