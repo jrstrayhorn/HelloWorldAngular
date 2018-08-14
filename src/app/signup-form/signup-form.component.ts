5016bd26573fef994c9c81d5ab6d4fe39bd1337f
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-14 10:21:00 -0400

    done with reactive forms, 11-nested formgroups

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -9,15 +9,18 @@ import { UsernameValidators } from './username.validators';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', 
-    [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace
-    ],
-    UsernameValidators.shouldBeUnique
-  ), // should use 'user-name' for key if using special characters
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+      username: new FormControl('', 
+      [
+        Validators.required,
+        Validators.minLength(3),
+        UsernameValidators.cannotContainSpace
+      ],
+      UsernameValidators.shouldBeUnique
+    ), // should use 'user-name' for key if using special characters
+      password: new FormControl('', Validators.required)
+    }),
+    
   });
 
   login() {
@@ -27,7 +30,7 @@ export class SignupFormComponent {
   }
 
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
 
