7de6d49ce09a592114f30f018ab56d904d848879
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-14 09:58:26 -0400

    done with reactive forms, 7-async operations

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -4,7 +4,18 @@ export class UsernameValidators {
     static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
         if ((control.value as string).indexOf(' ') != -1)
             return { cannotContainSpace: true }
+        return null;
+    }
 
+    static shouldBeUnique(control: AbstractControl) : ValidationErrors | null {
+        setTimeout(() => {
+            if (control.value == 'mosh')
+                return { shouldBeUnique: true }
+            return null;
+        }, 2000);
+
+        // this will not cause a typescript error but will not work because
+        // function will always return null
         return null;
     }
 }
