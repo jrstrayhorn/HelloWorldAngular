04dd6e293254fd1ffa2fb89eac14e1b9e6c32637
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-16 09:45:31 -0400

    done with routing and navigation, 11-subscribing to multiple observables

--- src/app/followers/followers.component.ts
+++ src/app/followers/followers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FollowersService } from '../services/followers.service';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs'
 
 @Component({
   selector: 'followers',
@@ -9,11 +11,41 @@ import { FollowersService } from '../services/followers.service';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
   
-  constructor(private service: FollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: FollowersService) { }
 
   ngOnInit() {
-    this.service.getAll()
+    combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    .subscribe(combined => {
+      let id = combined[0].get('id'); // dont have one setup
+      let page = combined[1].get('page');
+      let order = combined[1].get('order');
+      // use service to get data from the server
+      //this.service.getAll({ id, page });
+      console.log(id, page, order);
+
+      this.service.getAll()
       .subscribe(followers => this.followers = followers);
+    })
+
+    // Don't need the below lines because we're going to 
+    // combine observables above 
+    //obs.subscribe()
+    // this.route.paramMap
+    //   .subscribe(params => {
+
+    //   });
+
+    // this.route.queryParamMap
+    //   .subscribe(params => {
+
+    //   });
+
+ 
   }
 
 }
