5679b69a64d211da3b943a51e34cb37668c500bb
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-16 09:58:00 -0400

    done with routing and navigation, 12-the switchmap operator

--- src/app/followers/followers.component.ts
+++ src/app/followers/followers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FollowersService } from '../services/followers.service';
 import { ActivatedRoute } from '@angular/router';
 import { combineLatest } from 'rxjs'
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'followers',
@@ -20,32 +21,19 @@ export class GithubFollowersComponent implements OnInit {
       this.route.paramMap,
       this.route.queryParamMap
     ])
-    .subscribe(combined => {
-      let id = combined[0].get('id'); // dont have one setup
-      let page = combined[1].get('page');
-      let order = combined[1].get('order');
-      // use service to get data from the server
-      //this.service.getAll({ id, page });
-      console.log(id, page, order);
-
-      this.service.getAll()
-      .subscribe(followers => this.followers = followers);
-    })
-
-    // Don't need the below lines because we're going to 
-    // combine observables above 
-    //obs.subscribe()
-    // this.route.paramMap
-    //   .subscribe(params => {
-
-    //   });
-
-    // this.route.queryParamMap
-    //   .subscribe(params => {
-
-    //   });
-
- 
+    .pipe(
+      switchMap(combined => {
+        let id = combined[0].get('id'); // dont have one setup
+        let page = combined[1].get('page');
+        let order = combined[1].get('order');
+        // use service to get data from the server
+        //this.service.getAll({ id, page });
+        console.log(id, page, order);
+  
+        return this.service.getAll();
+      })
+    )
+    .subscribe(followers => this.followers = followers)
   }
 
 }
